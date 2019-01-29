553d5993802d971d0333d71a7f1770ceefc932c2
Vinny <vinny@heycharlie.org>
2019-01-28 19:12:58 -0500

    Improved turn cycle for when they go all the way around

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,6 @@
 
-# Simple TicTacToe game in Python - EAO
-
 import random
 import pandas as pd
-import sys
 
 size = 6
 board = pd.DataFrame(4, index=range(0, size), columns=['Player A', 'Player B'])
@@ -41,7 +38,7 @@ def print_board():
 def starting_position():
 	roles = ('Player A', 'Player B')
 	# if random.randint(0, 1) == 0:
-	# 	return chars[::-1]
+	# 	return roles[::-1]
 	return roles
 
 
@@ -51,68 +48,51 @@ def can_move(brd, player, move):
 	return False
 
 
-# def can_win(board, player, move):
-# 	places = []
-# 	x = 0
-# 	for i in board:
-# 		if i == player:
-# 			places.append(x)
-# 		x += 1
-# 	win = True
-# 	for tup in winners:
-# 		win = True
-# 		for ix in tup:
-# 			if board[ix] != player:
-# 				win = False
-# 				break
-# 		if win == True:
-# 			break
-# 	return win
+def turn_cycle(move, count, player):
+	other_player = board.columns.values[board.columns.values != player][0]
+	if move > 0:
+		increased_cells = list(range(max(0, move - count), move))
+		board.loc[board.index.isin(increased_cells), player] += 1  # Check this
+		count = count - move
+	if count < 1:  # If it doesn't make it to home
+		if (board.loc[min(increased_cells), player] == 1) and (
+				board.loc[size - 1 - min(increased_cells), other_player] > 0):
+			home.loc[player, 'Scores'] += board.loc[size - 1 - min(increased_cells), other_player] + 1
+			board.loc[min(increased_cells), player] = 0
+			board.loc[size - 1 - min(increased_cells), other_player] = 0
+		return True
+	elif count == 1:  # If it lands on home and gets another turn
+		home.loc[player, 'Scores'] += 1
+		if game_still_going():
+			if player == 'Player A':
+				print_board()
+				print('Move again!')
+				new_move = user_move()
+				return make_move(board, player, new_move)
+			else:  # player == 'Player B'
+				print('Computer moves again')
+				return computer_move()
+		else:
+			return True
+	else:  # count > 1 # If it passes home
+		home.loc[player, 'Scores'] += 1
+		count -= 1
+		if count > size:  # If it makes it all the way around
+			board[other_player] += 1
+			count -= size
+			return turn_cycle(size-1, count, player)
+		else:
+			for i in range(0, count):
+				board.loc[size - 1 - i, other_player] += 1
+			return True
 
 
 def make_move(board, player, move, undo=False):
-	other_player = board.columns.values[board.columns.values != player][0]
 	if can_move(board, player, move):
 		count = board.loc[move, player]
 		board.loc[move, player] = 0
-		if move > 0:
-			increased_cells = list(range(max(0, move - count), move))
-			board.loc[board.index.isin(increased_cells), player] += 1  # Check this
-			count = count - move
-		if count < 1:
-			# print('min of increased cells = ', min(increased_cells))
-			if (board.loc[min(increased_cells), player] == 1) and (board.loc[size - 1-min(increased_cells), other_player] > 0):
-				home.loc[player, 'Scores'] += board.loc[size - 1-min(increased_cells), other_player] + 1
-				board.loc[min(increased_cells), player] = 0
-				board.loc[size - 1 - min(increased_cells), other_player]= 0
-			return True
-		elif count == 1:
-			home.loc[player, 'Scores'] += 1
-			if game_still_going():
-				if player == 'Player A':
-					print_board()
-					print('Move again!')
-					new_move = user_move()
-					make_move(board, player, new_move)
-				else:  # player == 'Player B'
-					print('Computer moves again')
-					computer_move()
-			else:
-				return True
-		else:  # count > 1
-			home.loc[player, 'Scores'] += 1
-			count -= 1
-			if count > size:
-				board[other_player] += 1
-				count -= size
-				# cycle again with count, move = size - 1
-			else:
-				for i in range(0, count):
-					board.loc[size - 1-i, other_player] += 1
-		#
-		# if undo:
-		# 	board[move - 1] = move - 1
-		return True
+		return turn_cycle(move, count, player)
+		# return True
 	return False
 
 
@@ -168,16 +148,6 @@ while game_still_going():
 		continue
 	if game_still_going():
 		computer_move()
-	#
-	# if won:
-	# 	result = '*** Congratulations ! You won ! ***'
-	# 	break
-	# elif computer_move()[1]:
-	# 	result = '=== You lose ! =='
-	# 	break;
-
-# check for ties
-# result = home.idmax()
 
 print_board()
 print("game over")
